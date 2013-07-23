acf5f9078373247c654ad5261b4fb3237d79819e
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-07-23 19:54:31 -0400

    Some tiny more test cases to clarify construction of the planner object.

--- openrave_ompl_bridge/test/PluginTests.cpp
+++ openrave_ompl_bridge/test/PluginTests.cpp
@@ -7,6 +7,14 @@ TEST(PluginTests, Constructor)
 {
   OpenRAVE::RaveInitialize();
   OpenRAVE::EnvironmentBasePtr env = OpenRAVE::RaveCreateEnvironment();
+  // we can directly construct the planner object
   openrave_ompl_bridge::OMPLPlannerRRTConnect planner(env);  
-  EXPECT_TRUE(true);
+  // or ask OpenRAVE to do it in its factory
+  OpenRAVE::PlannerBasePtr planner_pointer = OpenRAVE::RaveCreatePlanner(env, "omplrrtconnect");  
+  // casting using boost should then be working
+  openrave_ompl_bridge::OMPLPlannerRRTConnectPtr planner_pointer2 = boost::static_pointer_cast<openrave_ompl_bridge::OMPLPlannerRRTConnect>(planner_pointer);  
+
+  EXPECT_TRUE(planner_pointer);
+  EXPECT_TRUE(planner_pointer2);
+  OpenRAVE::RaveDestroy();
 }
